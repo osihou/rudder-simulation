81b2722f8ec1877d5c9f117f5e874458bb9881bf
osihou <osihou@protonmail.com>
2021-05-16 11:14:07 +0200

    pid is now working

--- main.m
+++ main.m
@@ -11,12 +11,12 @@ clear; close all; clc;
 %SCA=0.5808;        % m
 y = 0.02323;        % m
 x = 0.04981;        % m
-delta0 = 25;        % deg
+delta0 = 25.1;        % deg
 
 x_min=-0.00515061;  % m
-x_max=0.0145;       % m
+x_max=0.0146;       % m
 
-dx=(x_min:0.00001:x_max);
+dx=(x_min:0.000005:x_max);
 
 delta = geometry(y, x, dx, delta0);
 
@@ -25,7 +25,7 @@ clear y x delta0 x_min x_max;
 %------LOADS-------
 
 rho = 1.2255;   % kg/m^3
-S = 0.1 * 1.965;      % m^2
+S =  1.965;      % m^2
 
 v_min = 100;    % m/s
 v_max = 300;    % m/s
@@ -37,28 +37,29 @@ Q =  actuator_load(rho, S, delta, v);
 clear rho S v_min v_max;
 
 %-------ACTUATOR-----
-m_actuator = 0.001;    % weight of actuator
-K = 500000;             % bulk modulus
 
-% A1 = 0.000490864;   % Area 1 m^2
-% A2 = 0.000038485;   % Area 2 m^2
-% 
-% V01 = 0.00000687;      % Volume 1 m^3
-% V02 = 0.0000008274177;   % Volume 2 m^3
-A1 = 0.00490864;   % Area 1 m^2
-A2 = 0.00038485;   % Area 2 m^2
+m_actuator = 1;    % weight of actuator
+K = 14000000000;             % bulk modulus
 
-V01 = 0.0000687;      % Volume 1 m^3
-V02 = 0.000008274177;   % Volume 2 m^3
+A1 = 0.000490864;   % Area 1 m^2
+A2 = 0.000038485;   % Area 2 m^2
 
-% A1 = 4.90874e-4;   % Area 1 m^2
-% A2 = 3.0e-4;   % Area 2 m^2
-% 
-% V01 = 2.5283e-6;      % Volume 1 m^3
-% V02 = 4.35e-6;   % Volume 2 m^3
+V01 = 0.00000687;      % Volume 1 m^3
+V02 = 0.0000008274177;   % Volume 2 m^3
 
-% A1 = 2;   % Area 1 m^2
-% A2 = 1;   % Area 2 m^2
-% 
-% V01 = 2;      % Volume 1 m^3
-% V02 = 1;   % Volume 2 m^3
+%--------PID--------
+
+P = 0.015;      % Proportional
+I = 0;          % Integral
+D = 0;          % Derivative
+
+N = 1;          % Filter coefficient
+
+%-------SENSOR------
+
+sgain = 0.01;
+smean = 0;
+svariance = 1;
+sseed = 0;
+ssample_time = 0.001;
+
